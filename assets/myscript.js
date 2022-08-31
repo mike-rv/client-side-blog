cf76041fb0dfea798b4d4339c2c33d2cb8b1aff0
shaeeb <shaeebali@gmail.com>
2022-08-31 10:42:07 +0100

    add giphy api functionality

--- assets/myscript.js
+++ assets/myscript.js
@@ -3,6 +3,7 @@ const btn = document.querySelector('.button-post');
 const p = document.querySelector('p');
 const form = document.querySelector('form');
 const input = document.querySelector('#post');
+const containerHistory = document.querySelector('.container-post-history')
 
 btn.addEventListener('click', () => {
     p.textContent = "No posts recorded";
@@ -36,38 +37,38 @@ thumbsDownEmoji.addEventListener('click', e => {
 })
 
 //giphy API:
-// let APIKEY = "txsMIouNt5mOzBbVoneiBmy1yjhXwYub"
+let APIKEY = "txsMIouNt5mOzBbVoneiBmy1yjhXwYub"
 
-// document.addEventListener("DOMContentLoaded", init);
-// function init() {
-//   document.getElementById("btnSearch").addEventListener("click", ev => {
-//     ev.preventDefault(); //to stop the page reload
-//     let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
-//     let str = document.getElementById("search").value.trim();
-//     url = url.concat(str);
-//     console.log(url);
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(content => {
-//         //  data, pagination, meta
-//         console.log(content.data);
-//         console.log("META", content.meta);
-//         let fig = document.createElement("figure");
-//         let img = document.createElement("img");
-//         let fc = document.createElement("figcaption");
-//         img.src = content.data[0].images.downsized.url;
-//         img.alt = content.data[0].title;
-//         fc.textContent = content.data[0].title;
-//         fig.appendChild(img);
-//         fig.appendChild(fc);
-//         let out = document.querySelector(".out");
-//         section.insertAdjacentElement("afterbegin", fig);
-//         document.querySelector("#search").value = "";
-//       })
-//       .catch(err => {
-//         console.error(err);
-//       });
-//   });
-// }
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+  document.getElementById("btnSearch").addEventListener("click", ev => {
+    ev.preventDefault(); //to stop the page reload
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+    let str = document.getElementById("search").value.trim();
+    url = url.concat(str);
+    console.log(url);
+    fetch(url)
+      .then(response => response.json())
+      .then(content => {
+        //  data, pagination, meta
+        console.log(content.data);
+        console.log("META", content.meta);
+        let fig = document.createElement("figure");
+        let img = document.createElement("img");
+        let fc = document.createElement("figcaption");
+        img.src = content.data[0].images.downsized.url;
+        img.alt = content.data[0].title;
+        fc.textContent = content.data[0].title;
+        fig.appendChild(img);
+        fig.appendChild(fc);
+        let out = document.querySelector(".out");
+        containerHistory.insertAdjacentElement("afterbegin", fig);
+        document.querySelector("#search").value = "";
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+}
 
 
